11d2aad77004d04a2c99615b6d02c6507d42f56e
Frank Laercio <franklaercio@gmail.com>
2021-03-14 06:27:05 -0300

    Adding Quicksort

---
+++ SortingAlgorithms/src/Main.java
@@ -0,0 +1,76 @@
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Random;
+
+/** Entity responsible for executing the sorting algorithms
+ *
+ * @author Frank Laércio, Ohanna Dezidério
+ * @since 2021-03-13
+ */
+public class Main {
+    public static void main(String[] args) {
+        int[] instances = {5, 10, 20, 40, 80, 160, 320, 640, 1280, 2560};
+
+        System.out.println("Quicksort for sorting numbers:");
+
+        for (int i = 0; i < instances.length; i++) {
+            int[] orderNumbers = sortedNumbers(instances[i]);
+
+            Instant startTimeQuicksort = Instant.now();
+
+            Quicksort.sorterNumbers(orderNumbers);
+
+            System.out.print(Duration.between(startTimeQuicksort, Instant.now()).toNanos() + " ns for ");
+            System.out.print(instances[i] + " numbers\n");
+        }
+
+        System.out.println("\nQuicksort for random numbers:");
+
+        for (int i = 0; i < instances.length; i++) {
+            int[] orderNumbers = randomNumbers(instances[i]);
+
+            Instant startTimeQuicksort = Instant.now();
+
+            Quicksort.sorterNumbers(orderNumbers);
+
+            System.out.print(Duration.between(startTimeQuicksort, Instant.now()).toNanos() + " ns for ");
+            System.out.print(instances[i] + " numbers\n");
+        }
+    }
+
+    /** Function responsible for creating ordered numbers
+     *
+     * @author Frank Laércio, Ohanna Dezidério
+     * @since 2021-03-13
+     *
+     * @param size
+     */
+    private static int[] sortedNumbers(int size) {
+        int[] numbers = new int[size];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i;
+        }
+
+        return numbers;
+    }
+
+    /** Function responsible for creating random numbers
+     *
+     * @author Frank Laércio, Ohanna Dezidério
+     * @since 2021-03-13
+     *
+     * @param size
+     */
+    private static int[] randomNumbers(int size) {
+        int[] numbers = new int[size];
+
+        Random random = new Random();
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt(size);
+        }
+
+        return numbers;
+    }
+}
---
+++ SortingAlgorithms/src/Quicksort.java
@@ -0,0 +1,74 @@
+/** Entity responsible for sorting numbers
+ *
+ * This algorithm is Quicksort, was created in 1960 by C.A.R. Hoare.
+ * In this case used one pivot for compare numbers.
+ * Once the pivot is defined, it is possible to know who the elements are smaller than him and bigger than him.
+ * Its complexity in the worst case is C(n) = O(n^2)
+ * Its complexity in the best case is C(n) = O(n log n)
+ *
+ * @author Frank Laércio, Ohanna Dezidério
+ * @since 2021-03-13
+ */
+public class Quicksort {
+
+    /** Function public for use Quicksort
+     *
+     * @param numbers
+     */
+    public static void sorterNumbers(int[] numbers) {
+        quickSort(numbers, 0, numbers.length - 1);
+    }
+
+    /** Function for sorting elements.
+     * In this case we going to get pivot.
+     * Call with recursive way quickSort for numbers successors and predecessors of the pivot.
+     *
+     * @param numbers
+     * @param start
+     * @param end
+     */
+    private static void quickSort(int[] numbers, int start, int end) {
+        if(start < end) {
+            int pivot = partition(numbers, start, end);
+
+            quickSort(numbers, start, pivot - 1);
+            quickSort(numbers, pivot + 1, end);
+        }
+    }
+
+    /** Function for divide elements.
+     * In this case we going to compare pivot with next number.
+     * Case the element was smaller than pivot.
+     *
+     * @param numbers
+     * @param start
+     * @param end
+     */
+    private static int partition(int[] numbers, int start, int end) {
+        int pivot = numbers[end];
+        int index = start;
+
+        for (int j = start; j < end; j++) {
+            if(numbers[j] < pivot) {
+                swap(numbers, index, j);
+                index += 1;
+            }
+        }
+
+        swap(numbers, index, end);
+
+        return index;
+    }
+
+    /** Function for change elements.
+     *
+     * @param numbers
+     * @param start
+     * @param end
+     */
+    private static void swap(int[] numbers, int start, int end) {
+        int aux = numbers[start];
+        numbers[start] = numbers[end];
+        numbers[end] = aux;
+    }
+}
